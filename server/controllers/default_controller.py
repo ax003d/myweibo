f374fd34d4a1b51d311e5bc4ad79620c8ac381b6
Robert Zheng <rzheng@xtremeprog.com>
2016-07-09 16:42:31 +0800

    py3 write bytes to file

--- server/controllers/default_controller.py
+++ server/controllers/default_controller.py
@@ -3,7 +3,8 @@ from settings import DATA_PATH
 
 
 def v1_tagignore_post(body):
-    with open(os.path.join(DATA_PATH, 'tagignore'), 'a') as f:
-        f.write('\n')
-        f.writelines('\n'.join(body['tagignore']))
+    with open(os.path.join(DATA_PATH, 'tagignore'), 'ab') as f:
+        for i in body['tagignore']:
+            f.write(i.encode())
+            f.write('\n'.encode())
     return ''
