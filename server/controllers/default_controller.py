bfa301a42835109979618867a5b8d3ff3cf5df77
Robert Zheng <rzheng@xtremeprog.com>
2016-07-09 14:07:29 +0800

    append tagignore

--- server/controllers/default_controller.py
+++ server/controllers/default_controller.py
@@ -1,3 +1,16 @@
+import json
+import os
+import dotenv
+
+dotenv.read_dotenv()
+
+from getenv import env
+
+DATA_PATH = env("DATA_PATH", "/data")
+
 
 def v1_tagignore_post(body):
-    return 'do some magic!'
+    with open(os.path.join(DATA_PATH, 'tagignore'), 'a') as f:
+        f.write('\n')
+        f.writelines('\n'.join(body['tagignore']))
+    return json.dumps({})
